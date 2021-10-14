c042302e31455e9417508132babd5d7e45e9d984
Bach <dalastraz1712@gmail.com>
2021-10-14 17:04:35 +0700

    last

--- JAVA/lesson1/Hello World/src/App.java
+++ JAVA/lesson1/Hello World/src/App.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
-public class App {
-    public static void main(String[] args) throws Exception {
+public class App {public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         System.out.println("Nhập cân nặng theo kg: ");
         float weight = sc.nextFloat();
         System.out.println("Nhập chiều cao theo m: ");
         float height = sc.nextFloat();
 
-        float bmi = weight / (height * height);
+        // float bmi = weight / (height * height);
         // System.out.println("BMI là: "+ bmi);
         System.out.println("BMI là: " + weight / (height * height));
+        sc.close();
     }
 }
